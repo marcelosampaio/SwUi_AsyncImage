881c3a7b731576f574209697a53e5f4c31c02bf6
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-11-11 18:56:49 -0300

    AsyncImage in action

--- SwUi_AsyncImage/ContentView.swift
+++ SwUi_AsyncImage/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        AsyncImage(url: URL(string: "https://blckbirds.com/wp-content/uploads/2021/10/pexels-kammeran-gonzalezkeola-6128227-2.jpg"), transaction: .init(animation: .spring(response: 1.6))) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+                    .progressViewStyle(.circular)
+            case .success(let image):
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            case .failure:
+                Text("Failed fetching image. Make sure to check your data connection and try again.")
+                    .foregroundColor(.red)
+            @unknown default:
+                Text("Unknown error. Please try again.")
+                    .foregroundColor(.red)
+            }
         }
-        .padding()
+            .frame(height: 240)
     }
 }
 
