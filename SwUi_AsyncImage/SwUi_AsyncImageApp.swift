08bf1497ea25561a6559089e4cd1a5052b1faa2c
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-11-11 18:13:20 -0300

    Initial Commit

---
+++ SwUi_AsyncImage/SwUi_AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_AsyncImageApp.swift
+//  SwUi_AsyncImage
+//
+//  Created by Marcelo Sampaio on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
